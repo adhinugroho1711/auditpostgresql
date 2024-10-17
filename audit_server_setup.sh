6134737e18b59f899e6db1d8a7a8fb138f00ba0d
P Adhi Nugroho <phenom1711@gmail.com>
2024-10-17 20:46:06 +0700

    Update audit_server_setup.sh

--- audit_server_setup.sh
+++ audit_server_setup.sh
@@ -28,8 +28,8 @@ EOF
 # Fungsi untuk mengkonfigurasi PostgreSQL untuk remote access
 configure_postgresql() {
     echo "Configuring PostgreSQL for remote access..."
-    sudo sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/" /etc/postgresql/12/main/postgresql.conf
-    echo "host    all             all             0.0.0.0/0               md5" | sudo tee -a /etc/postgresql/12/main/pg_hba.conf
+    sudo sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/" /etc/postgresql/14/main/postgresql.conf
+    echo "host    all             all             0.0.0.0/0               md5" | sudo tee -a /etc/postgresql/14/main/pg_hba.conf
     sudo systemctl restart postgresql || handle_error "Failed to restart PostgreSQL"
 }
 
