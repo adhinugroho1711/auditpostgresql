959f58fefb9edb9ac26151783e827e87c9c278da
P Adhi Nugroho <phenom1711@gmail.com>
2024-10-18 13:58:46 +0700

    fixing version

--- audit_server_setup.sh
+++ audit_server_setup.sh
@@ -54,7 +54,7 @@ EOF
 
 # Fungsi untuk mengkonfigurasi PostgreSQL untuk remote access
 configure_postgresql() {
-    local pg_version=$1
+    local version=$(sudo -u postgres psql -t -c "SHOW server_version_num;" | tr -d ' \n' | cut -c1-2)
     log_info "Configuring PostgreSQL version $pg_version for remote access..."
     sudo sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/" /etc/postgresql/$pg_version/main/postgresql.conf
     echo "host    all             all             0.0.0.0/0               md5" | sudo tee -a /etc/postgresql/$pg_version/main/pg_hba.conf
